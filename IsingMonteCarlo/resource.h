c3e9b37aea6e049dcc79ea6a355988570d2a8c9c
Adrian Roman <aroman_ro@yahoo.com>
2018-12-15 12:11:47 +0200

    Added links to GitHub repository and Blog description to About dialog

--- IsingMonteCarlo/resource.h
+++ IsingMonteCarlo/resource.h
@@ -84,6 +84,8 @@
 #define IDC_MFCCOLORBUTTON3             1008
 #define IDC_MFCCOLORBUTTON4             1009
 #define IDC_CHECK1                      1010
+#define IDC_MFCLINK1                    1011
+#define IDC_MFCLINK2                    1012
 #define ID_VIEW_RENORMALIZATION         32771
 #define ID_VIEW_OPTIONS                 32772
 #define ID_SIMULATE_RUN                 32773
@@ -92,9 +94,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        319
+#define _APS_NEXT_RESOURCE_VALUE        320
 #define _APS_NEXT_COMMAND_VALUE         32774
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
