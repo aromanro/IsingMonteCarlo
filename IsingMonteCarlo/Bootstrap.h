94bf4e6bbd4c753e753911f007df6c976404c5b2
Adrian Roman <a.roman@ib-company.de>
2021-08-28 20:55:38 +0300

    Added a Bootstrap class, not complete yet.

---
+++ IsingMonteCarlo/Bootstrap.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "StatisticsBinCollection.h"
+
+namespace Statistics
+{
+
+    // not complete yet
+
+    template<unsigned int N> class Bootstrap :
+        public StatisticsBinCollection<N>
+    {
+    public:
+        Bootstrap(long long SampleSize) : sampleSize(SampleSize), count(0), binExcl(sampleSize / N) {}
+
+        void Reset()
+        {
+            count = 0;
+            StatisticsBinCollection<N>::Reset();
+        }
+
+        void AddValue(double value)
+        {
+            for (int bin = 0; bin < N; ++bin)
+                if (bin != count / binExcl)
+                    m_bins[bin].AddValue(value);
+
+            ++count;
+        }
+    protected:
+        long long sampleSize;
+        long long int count;
+        long long binExcl;
+    };
+
+}
+
